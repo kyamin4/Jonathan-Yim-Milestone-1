9f66f83e5a622c730e7b6fb05a5213bba1bc67f3
Jonathan Yim <s_jonathan.yim@ousd.org>
2021-10-13 10:23:00 -0700

    did a lot of work, signup page, question page stuff, minor adjustments and some JS functionality?

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,3 +8,34 @@ $(function () {
     document.title = "your site name"
 
   })
+
+function questionEnter(){
+  var email = document.getElementById("question-user-email").value;
+  var username = document.getElementById("question-user-name").value;
+  var question = document.getElementById("question-input").value;
+  var comments = document.getElementById("question-comments").value;
+  var textbook = document.getElementById("question-textbook").value;
+  var subject = document.getElementById("question-subject").value;
+  var image = document.getElementById("question-image").value;
+
+  var fullQuestion = {
+    userEmail: email,
+    userName: username,
+    userQuestion: question,
+    userComments: comments,
+    userTextbook: textbook,
+    userSubject: subject,
+    userImage: image
+  }
+
+}
+
+function clearQuestionEntryFields(){
+  document.getElementById("question-user-email").value = "";
+  document.getElementById("question-user-name").value = "";
+  document.getElementById("question-input").value = "";
+  document.getElementById("question-comments").value = "";
+  document.getElementById("question-textbook").value = "";
+  document.getElementById("question-subject").value = "";
+  document.getElementById("question-image").value = "";
+}
