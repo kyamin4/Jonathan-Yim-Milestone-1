2d76df1ede4738f058a49ae77a552003e3692c86
Jonathan Yim <s_jonathan.yim@ousd.org>
2021-10-15 11:05:30 -0700

    fucking made button shit work omfg took forever

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -9,63 +9,76 @@ $(function () {
 
   })
 
+/*
+  var fullQuestion = {
+  }
+  
+function questionEnter(){
+var email = document.getElementById("question_user_email");
+var username = document.getElementById("question_user_name");
+var question = document.getElementById("question_input");
+var comments = document.getElementById("question_comments");
+var textbook = document.getElementById("question_textbook");
+var subject = document.getElementById("question_subject");
+var image = document.getElementById("question_file");
 
-var fullQuestion = {
-
+if(username == "" || username == null){
+  username = "Anonymous";
+}
+if(comments == "" || comments == null){
+  comments = "No comments.";
+}
+if(image == "" || image == null){
+  image  = "No image.";
+}
+if(subject=="Choose..."){
+  subject = "N/A or Other";
 }
 
-function questionEnter(){
-  var email = document.getElementById("question_user_email");
-  var username = document.getElementById("question_user_name");
-  var question = document.getElementById("question_input");
-  var comments = document.getElementById("question_comments");
-  var textbook = document.getElementById("question_textbook");
-  var subject = document.getElementById("question_subject");
-  var image = document.getElementById("question_file");
+fullQuestion = {
+  userEmail: email.value,
+  userName: username.value,
+  userQuestion: question.value,
+  userComments: comments.value,
+  userTextbook: textbook.value,
+  userSubject: subject.value,
+  userImage: image.value
+}
 
-  if(username == "" || username == null){
-    username = "Anonymous";
-  }
-  if(comments == "" || comments == null){
-    comments = "No comments."
-  }
-  if(image == "" || image == null){
-    image  = "No image."
-  }
-
-  fullQuestion = {
-    userEmail: email.value,
-    userName: username.value,
-    userQuestion: question.value,
-    userComments: comments.value,
-    userTextbook: textbook.value,
-    userSubject: subject.value,
-    userImage: image.value
-  }
-  console.log(fullQuestion);
+console.log(fullQuestion);
+console.log("Question entered!");
 }
 
 function clearQuestionEntryFields(){
-  document.getElementById("question_user_email").value = "";
-  document.getElementById("question_user_name").value = "";
-  document.getElementById("question_input").value = "";
-  document.getElementById("question_comments").value = "";
-  document.getElementById("question_textbook").value = "";
-  document.getElementById("question_subject").value = "";
-  document.getElementById("question_file").value = "";
+document.getElementById("question_user_email").value = "";
+document.getElementById("question_user_name").value = "";
+document.getElementById("question_input").value = "";
+document.getElementById("question_comments").value = "";
+document.getElementById("question_textbook").value = "";
+document.getElementById("question_subject").value = "";
+document.getElementById("question_file").value = "";
+console.log("Clear entry fields done!");
 }
 
-  $("#question_submit").click(function(){
-    console.log("Submit button clicked!")
-    questionEnter();
-  });
+document.getElementById("question_submit").addEventListener("mouseup", run_question_full());
+
+function run_question_full(){
+  console.log("Submit button clicked!")
+  questionEnter();
+  displayQuestion();
+  console.log(fullQuestion + "END");
+  clearQuestionEntryFields();
+}
 
 function displayQuestion(){
-    $("#question_results").append("Entered Email: " + fullQuestion.userEmail + '\n');
-    $("#question_results").append("Entered Username: " + fullQuestion.userName + '\n'); 
-    $("#question_results").append("Entered Question: " + fullQuestion.userQuestion + '\n'); 
-    $("#question_results").append("Entered Comments: " + fullQuestion.userComments + '\n'); 
-    $("#question_results").append("Entered Textbook: " + fullQuestion.userTextbook + '\n'); 
-    $("#question_results").append("Entered Subject: " + fullQuestion.userTextbook + '\n'); 
-    $("#question_results").append("Entered Image: " + fullQuestion.userImage + '\n'); 
+document.getElementById("question_results").append("Entered Email: " + fullQuestion.userEmail);
+document.getElementById("question_results").append("Entered Username: " + fullQuestion.userName); 
+document.getElementById("question_results").append("Entered Question: " + fullQuestion.userQuestion); 
+document.getElementById("question_results").append("Entered Comments: " + fullQuestion.userComments); 
+document.getElementById("question_results").append("Entered Textbook: " + fullQuestion.userTextbook); 
+document.getElementById("question_results").append("Entered Subject: " + fullQuestion.userTextbook); 
+document.getElementById("question_results").append("Entered Image: " + fullQuestion.userImage); 
+console.log("Display questions done!");
 }
+
+*/
